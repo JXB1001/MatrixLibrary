657e5c91439e62ce724409c89e93435d4c65e700
Jacob Bentley <bentley.silas@gmail.com>
2019-09-26 19:46:47 +0100

    Created Matrix and Vector Classes

---
+++ MatrixLibrary/MatrixLibrary/Matrix.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixLibrary
+{
+    public class Matrix
+    {
+        private int height;
+        private int width;
+        private int size;
+        private long[] data;
+
+        public Matrix(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+            this.size = height * width;
+            this.data = new long[this.size];
+        }
+
+
+        public long Get(int y, int x)
+        {
+            return this.data[y * this.height + x];
+        }
+
+        public void Set(int y, int x, long value)
+        {
+            this.data[y * this.height + x] = value;
+        }
+
+        public IEnumerable<Vector> Horizontal()
+        {
+            Vector output = new Vector(this.width);
+            for (int y = 0; y < this.height; y++)
+            {
+                for (int x = 0; x < this.width; x++)
+                {
+                    output.Set(x, Get(y, x));
+                }
+                yield return output;
+            }
+        }
+
+        public IEnumerable<Vector> Vertical()
+        {
+            Vector output = new Vector(this.height);
+            for (int x = 0; x < this.width; x++)
+            {
+                for (int y = 0; y < this.height; y++)
+                {
+                    output.Set(y, Get(y, x));
+                }
+                yield return output;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (Vector vector in Horizontal())
+            {
+                stringBuilder.Append(vector.ToString());
+                stringBuilder.Append("\n");
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ MatrixLibrary/MatrixLibrary/Vector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixLibrary
+{
+    public class Vector
+    {
+        private int size;
+        private long[] data;
+
+        public Vector(int size)
+        {
+            this.size = size;
+            this.data = new long[this.size];
+        }
+
+        public long Get(int i)
+        {
+            return this.data[i];
+        }
+
+        public void Set(int i, long value)
+        {
+            this.data[i] = value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (long l in this.data)
+            {
+                stringBuilder.Append($"{l:10}");
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
